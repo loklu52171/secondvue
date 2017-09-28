a041c1dd7786e1eff2add2905c2065060c2ca37b
李璐 <231614985@qq.com>
2017-09-28 16:48:17 +0800

    下午修改

--- src/components/api/home.js
+++ src/components/api/home.js
@@ -1,17 +1,31 @@
-import axios from 'axios'
+import jsonp from 'src/style/js/jsonp'
+import { commonParams, options } from './config'
+// import axios from 'axios'
 
-export function getKuaiList () {
-  const url = 'https://restapi.ele.me/shopping/v2/entries'
+export function getSliderData () { // 获取轮播图数据
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
 
-  const data = Object.assign({}, {
-    latitude: 28.227779,
-    longitude: 112.938858,
-    limit: 20
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
   })
-
-  return axios.get(url, {
-    params: data
-  }).then((res) => {
-    return Promise.resolve(res.data)
-  })
+  return jsonp(url, data, options)
 }
+
+// export function getKuaiList () {
+//   const url = 'https://restapi.ele.me/shopping/v2/entries'
+
+//   const data = Object.assign({}, {
+//     latitude: 28.227779,
+//     longitude: 112.938858,
+//     limit: 20
+//   })
+
+//   return axios.get(url, {
+//     params: data
+//   }).then((res) => {
+//     return Promise.resolve(res.data)
+//   })
+// }
+
