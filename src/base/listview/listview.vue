2029417d1b17b214c60fb2212eb80b7d0ba14761
李璐 <231614985@qq.com>
2017-10-16 16:32:10 +0800

    做好了联系人

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,241 @@
+<template>
+    <scroll @scroll="scroll" :listen-scroll="listenScroll" :probe-type="probeType" :data="data" class="listview" ref="listview">
+        <ul>
+            <li v-for="group in data" class="list-group" ref="listGroup">
+                <h2 class="list-group-title">{{group.title}}</h2>
+                <uL>
+                    <li @click="selectItem(item)" v-for="item in group.items" class="list-group-item">
+                        <img class="avatar" v-lazy="item.avatar">
+                        <span class="name">{{item.name}}</span>
+                    </li>
+                </uL>
+            </li>
+        </ul>
+        <div class="list-shortcut" @touchstart.stop.prevent="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove" @touchend.stop>
+            <ul>
+                <li v-for="(item, index) in shortcutList" :data-index="index" class="item" :class="{'current':currentIndex===index}">{{item}}
+                </li>
+            </ul>
+        </div>
+        <div class="list-fixed" ref="fixed" v-show="fixedTitle">
+            <div class="fixed-title">{{fixedTitle}} </div>
+        </div>
+        <div v-show="!data.length" class="loading-container">
+            <loading></loading>
+        </div>
+    </scroll>
+</template>
+
+<script type="text/ecmascript-6">
+import Scroll from 'src/base/scroll/scroll'
+import Loading from 'src/base/loading/loading'
+import { getData } from 'src/style/js/dom'
+
+const TITLE_HEIGHT = 30
+const ANCHOR_HEIGHT = 18
+
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: []
+    }
+  },
+  computed: {
+    shortcutList() {
+      return this.data.map((group) => {
+        return group.title.substr(0, 1)
+      })
+    },
+
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
+    }
+  },
+  data() {
+    return {
+      scrollY: -1,
+      currentIndex: 0,
+      diff: -1
+    }
+  },
+  created() {
+    this.probeType = 3
+    this.listenScroll = true
+    this.touch = {}
+    this.listHeight = []
+  },
+  methods: {
+    selectItem(item) {
+      this.$emit('select', item)
+    },
+    onShortcutTouchStart(e) {
+      let anchorIndex = getData(e.target, 'index')
+      let firstTouch = e.touches[0]
+      this.touch.y1 = firstTouch.pageY
+      this.touch.anchorIndex = anchorIndex
+      this._scrollTo(anchorIndex)
+    },
+    onShortcutTouchMove(e) {
+      let firstTouch = e.touches[0]
+      this.touch.y2 = firstTouch.pageY
+      let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
+      let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+      this._scrollTo(anchorIndex)
+    },
+    refresh() {
+      this.$refs.listview.refresh()
+    },
+    scroll(pos) {
+      this.scrollY = pos.y
+    },
+    _calculateHeight() {
+      this.listHeight = []
+      const list = this.$refs.listGroup
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < list.length; i++) {
+        let item = list[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
+    },
+    _scrollTo(index) {
+      if (!index && index !== 0) {
+        return
+      }
+      if (index < 0) {
+        index = 0
+      } else if (index > this.listHeight.length - 2) {
+        index = this.listHeight.length - 2
+      }
+      this.scrollY = -this.listHeight[index]
+      this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+    }
+  },
+  watch: {
+    data() {
+      setTimeout(() => {
+        this._calculateHeight()
+      }, 20)
+    },
+    scrollY(newY) {
+      const listHeight = this.listHeight
+      // 当滚动到顶部，newY>0
+      if (newY > 0) {
+        this.currentIndex = 0
+        return
+      }
+      // 在中间部分滚动
+      for (let i = 0; i < listHeight.length - 1; i++) {
+        let height1 = listHeight[i]
+        let height2 = listHeight[i + 1]
+        if (-newY >= height1 && -newY < height2) {
+          this.currentIndex = i
+          this.diff = height2 + newY
+          return
+        }
+      }
+      // 当滚动到底部，且-newY大于最后一个元素的上限
+      this.currentIndex = listHeight.length - 2
+    },
+    diff(newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+      if (this.fixedTop === fixedTop) {
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
+    }
+  },
+  components: {
+    Scroll,
+    Loading
+  }
+}
+
+</script>
+
+<style lang="scss">
+.listview {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background: #222;
+    .list-group {
+        padding-bottom: 0.3rem;
+        .list-group-title {
+            height: 0.5rem;
+            line-height: 0.5rem;
+            padding-left: 0.3rem;
+            font-size: 0.24rem;
+            color: rgba(255, 255, 255, 0.5);
+            background: #333;
+        }
+
+        .list-group-item {
+            display: flex;
+            align-items: center;
+            padding: 0.5rem 0 0 0.5rem;
+            .avatar {
+                width: 0.8rem;
+                height: 0.8rem;
+                border-radius: 50%;
+            }
+            .name {
+                margin-left: 0.5rem;
+                color: rgba(255, 255, 255, 0.5);
+                font-size: 0.2rem;
+            }
+        }
+    }
+
+    .list-shortcut {
+        position: absolute;
+        z-index: 30;
+        right: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 0.5rem;
+        padding: 0.24rem 0;
+        border-radius: 0.1rem;
+        text-align: center;
+        background: rgba(0, 0, 0, 0.3);
+        font-family: Helvetica;
+        .item {
+            padding: 0.03rem;
+            line-height: 1;
+            color:#eee;
+            font-size: 0.2rem;
+            margin-bottom: 0.06rem;
+            &.current {
+                color: #ffcd32;
+            }
+        }
+    }
+    .list-fixed {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        .fixed-title {
+            height: 0.5rem;
+            line-height: 0.5rem;
+            padding-left: 0.3rem;
+            font-size: 0.24rem;
+            color: yellow;
+            background: #333;
+        }
+    }
+    .loading-container {
+        position: absolute;
+        width: 100%;
+        top: 40%;
+        transform: translateY(-40%);
+    }
+}
+</style>
--- src/components/discount/discount.vue
+++ src/components/discount/discount.vue
@@ -1,13 +1,101 @@
 <template>
-  <div>
-      优惠111
+  <div class="singer" ref="singer">
+    <list-view @select="selectSinger" :data="singers" ref="list"></list-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import ListView from 'src/base/listview/listview'
+import { getSingerList } from 'src/components/api/singer'
+import { ERR_OK } from 'src/components/api/config'
+import Singer from 'src/style/js/singer'
+import { mapMutations } from 'vuex'
 
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
+
+export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    selectSinger(singer) {
+      this.$router.push({
+        path: `/singer/${singer.id}`
+      })
+      this.setSinger(singer)
+    },
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
+        }
+      })
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid
+        }))
+      })
+      // 为了得到有序列表，我们需要处理 map
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    },
+    ...mapMutations({
+      setSinger: 'SET_SINGER'
+    })
+  },
+  components: {
+    ListView
+  }
+}
 </script>
 
 <style lang="scss">
-
+  .singer{
+    position: fixed;
+    top: 0.9rem;
+    bottom: 0;
+    width: 100%;
+  }
+    
 </style>
